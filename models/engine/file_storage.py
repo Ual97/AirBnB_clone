eef898e0df804857eb957cfb7b7c7e5a0bea3f7a
Diekkan <merentiel.diego@gmail.com>
2022-03-01 13:49:02 -0300

    File Storage Class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,5 +6,24 @@ into instances.
 
 
 class FileStorage:
-    """ Class that helps serializing and deserializing objects """
-    
+    """ Class that works serializing and deserializing objects 
+    from JSON and to JSON.
+
+    Att:
+    __file_path: string - path to the JSON file
+    __objects: dictionary - empty but will store all objects by <class name>.id
+
+    Methods:
+    all(self): returns the dictionary __objects
+    new(self, obj): sets in __objects the obj with key <obj class name>.id
+    save(self): serializes __objects to the JSON file (path: __file_path)
+    reload(self): deserializes the JSON file to __objects
+     (only if the JSON file (__file_path) exists ; otherwise, do nothing.
+    If the file doesn't exist, no exception should be raised)
+
+    """
+    __file_path = ""
+    __objects = {}
+
+    def all(self):
+        pass
