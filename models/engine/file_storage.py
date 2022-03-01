aefb7003c6517c3154ef0b4b241cdb032fb6837d
Diekkan <merentiel.diego@gmail.com>
2022-03-01 13:46:46 -0300

    File Storage class

---
+++ models/engine/file_storage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Serializing instances to a JSON file and deserializing JSON files
+into instances.
+"""
+
+
+class FileStorage:
+    """ Class that helps serializing and deserializing objects """
+    
