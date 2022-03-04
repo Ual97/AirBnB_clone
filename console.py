caa3651a417a86e86cff2a90bf61e592f0c546e0
Diekkan <merentiel.diego@gmail.com>
2022-03-04 14:33:59 -0300

    Testing console

--- console.py
+++ console.py
@@ -2,28 +2,20 @@
 """Program that contains entry point of the command interpreter"""
 
 import cmd
-from models import storage
-from models.amenity import Amenity
-from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 
 class HBNBCommand(cmd.Cmd):
     """command interpreter class"""
-    prompt = "(hbnb)"
+    prompt = "(hbnb) "
     classes = ["BaseModel", "User", "City", "Amenity",
                "Review", "State", "Place"]
 
-    def do_EOF(self):
+    def do_EOF(self, line):
         """ End of file command """
         print()
         return True
 
-    def do_quit(self):
+    def do_quit(self, line):
         """ Command to quit the CMD interface """
         print()
         return True
